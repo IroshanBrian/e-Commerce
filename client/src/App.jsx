83a4130d9cb8ec76b7fc827de7a401bc6b763b25
Iroshan Brian Bandaragoda <brianstudiolk@gmail.com>
2024-10-20 03:23:12 +0530

    chore: Add Tailwind CSS configuration and styles
    Added the necessary configuration files and stylesheets for integrating Tailwind CSS into the project. This includes the `index.css` file, `postcss.config.js`, and `tailwind.config.js`. These changes will allow us to use Tailwind CSS utility classes and components throughout the application.

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
+
+function App() {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
