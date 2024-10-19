2b9f719c02089be147ea79ef748572fd08ea9aaa
Iroshan Brian Bandaragoda <brianstudiolk@gmail.com>
2024-10-20 02:29:54 +0530

    chore: Add payment routes and controller

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth.route.js';
 import cartRoutes from './routes/cart.route.js';
 import productsRoutes from './routes/product.route.js';
 import couponsRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponsRoutes);
+app.use('/api/payments', paymentRoutes);
 
 app.listen(PORT, () => {
      console.log(`Server is running on ${PORT}`);
